c41f5601a1b0ae76aa787e3b85faca8fa0a3b2ae
mosesintech <mosesmaxcosme@gmail.com>
2025-01-01 18:24:11 -0600

    feat(ui): add logic to display one friend info and to return to all friends in Journal

--- src/Managers/UIManager.ts
+++ src/Managers/UIManager.ts
@@ -365,7 +365,7 @@ class UIManager {
 
   private createJournalFriends() {
     const journal_friends = document.getElementById('journal_friends')!;
-    const journal_friends_default = `
+    journal_friends.innerHTML = `
       <div id="friends_container" class="friends_container">
         <div class="friends_header">
           <h2>Journal Friends</h2>
@@ -681,22 +681,101 @@ class UIManager {
       <div id="friend_single_city1_andrew_info" class="friend_single_city1_andrew_info"></div>
   `;
 
-    journal_friends.innerHTML = journal_friends_default;
     const see_all_friends = document.getElementById('see_all_friends')!;
+    const friend_single_city1_andrew_info = document.getElementById(
+      'friend_single_city1_andrew_info'
+    )!;
 
     const friend_single_city1_andrew = document.getElementById(
       'friend_single_city1_andrew'
     )!;
+    const friend_single_city1_catherine = document.getElementById(
+      'friend_single_city1_catherine'
+    )!;
+    const friend_single_city1_jibblet = document.getElementById(
+      'friend_single_city1_jibblet'
+    )!;
+    const friend_single_city1_jonathan = document.getElementById(
+      'friend_single_city1_jonathan'
+    )!;
+    const friend_single_city1_jt = document.getElementById(
+      'friend_single_city1_jt'
+    )!;
+    const friend_single_city1_karath = document.getElementById(
+      'friend_single_city1_karath'
+    )!;
+    const friend_single_city1_matty = document.getElementById(
+      'friend_single_city1_matty'
+    )!;
+    const friend_single_city1_moses = document.getElementById(
+      'friend_single_city1_moses'
+    )!;
+    const friend_single_city1_nektarios = document.getElementById(
+      'friend_single_city1_nektarios'
+    )!;
+    const friend_single_city1_newberry = document.getElementById(
+      'friend_single_city1_newberry'
+    )!;
+    const friend_single_city1_orpheus = document.getElementById(
+      'friend_single_city1_orpheus'
+    )!;
+    const friend_single_city1_ryan = document.getElementById(
+      'friend_single_city1_ryan'
+    )!;
+    const friend_single_city1_tanner = document.getElementById(
+      'friend_single_city1_tanner'
+    )!;
+    const friend_single_city1_tsubaki = document.getElementById(
+      'friend_single_city1_tsubaki'
+    )!;
+    const friend_single_city1_tsuki = document.getElementById(
+      'friend_single_city1_tsuki'
+    )!;
+    const friend_single_city1_victor = document.getElementById(
+      'friend_single_city1_victor'
+    )!;
     friend_single_city1_andrew.onclick = function () {
-      const journal_friends_andrew_bio = ``;
+      friend_single_city1_andrew_info.innerHTML = `testing`;
 
       const see_all_friends = document.getElementById('see_all_friends')!;
       see_all_friends.style.display = 'block';
-      // journal_friends.innerHTML = journal_friends_andrew_bio;
+      friend_single_city1_andrew.style.display = 'none';
+      friend_single_city1_catherine.style.display = 'none';
+      friend_single_city1_jibblet.style.display = 'none';
+      friend_single_city1_jonathan.style.display = 'none';
+      friend_single_city1_jt.style.display = 'none';
+      friend_single_city1_karath.style.display = 'none';
+      friend_single_city1_matty.style.display = 'none';
+      friend_single_city1_moses.style.display = 'none';
+      friend_single_city1_nektarios.style.display = 'none';
+      friend_single_city1_newberry.style.display = 'none';
+      friend_single_city1_orpheus.style.display = 'none';
+      friend_single_city1_ryan.style.display = 'none';
+      friend_single_city1_tanner.style.display = 'none';
+      friend_single_city1_tsubaki.style.display = 'none';
+      friend_single_city1_tsuki.style.display = 'none';
+      friend_single_city1_victor.style.display = 'none';
     };
 
     see_all_friends.onclick = function () {
       see_all_friends.style.display = 'none';
+      friend_single_city1_andrew.style.display = 'block';
+      friend_single_city1_catherine.style.display = 'block';
+      friend_single_city1_jibblet.style.display = 'block';
+      friend_single_city1_jonathan.style.display = 'block';
+      friend_single_city1_jt.style.display = 'block';
+      friend_single_city1_karath.style.display = 'block';
+      friend_single_city1_matty.style.display = 'block';
+      friend_single_city1_moses.style.display = 'block';
+      friend_single_city1_nektarios.style.display = 'block';
+      friend_single_city1_newberry.style.display = 'block';
+      friend_single_city1_orpheus.style.display = 'block';
+      friend_single_city1_ryan.style.display = 'block';
+      friend_single_city1_tanner.style.display = 'block';
+      friend_single_city1_tsubaki.style.display = 'block';
+      friend_single_city1_tsuki.style.display = 'block';
+      friend_single_city1_victor.style.display = 'block';
+      friend_single_city1_andrew_info.innerHTML = ``;
     };
   }
 
