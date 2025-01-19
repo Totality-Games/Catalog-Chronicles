60d18413bb14ca0fe7c5ac75548f5533371b317b
mosesintech <mosesmaxcosme@gmail.com>
2025-01-18 21:52:34 -0600

    feat(ui): add missing Friend detail pages

--- src/Managers/helpers/friends/city1/city1Details.ts
+++ src/Managers/helpers/friends/city1/city1Details.ts
@@ -1,3 +1,4 @@
+import { CITY1_FRIENDS } from '../../../../constants';
 import friendDetails from '../friendDetails';
 import hideFriendRows from '../hideFriendRows';
 
@@ -106,27 +107,143 @@ export default function city1Details() {
 
   // hides friend rows and shows single friend details when clicked
   friend_single_city1_andrew.onclick = function () {
-    friendDetails('Andrew', '/images/npcs/city1/catherine.png').then((res) => {
+    friendDetails(
+      CITY1_FRIENDS.ANDREW,
+      '/images/npcs/city1/catherine.png'
+    ).then((res) => {
+      hideFriendRows();
       return (friend_single_city1_andrew_info.innerHTML = res);
     });
-
-    hideFriendRows();
   };
   friend_single_city1_catherine.onclick = function () {
-    friendDetails('Catherine', '/images/npcs/city1/catherine.png').then(
-      (res) => {
-        return (friend_single_city1_catherine_info.innerHTML = res);
-      }
-    );
-
-    hideFriendRows();
+    friendDetails(
+      CITY1_FRIENDS.CATHERINE,
+      '/images/npcs/city1/catherine.png'
+    ).then((res) => {
+      hideFriendRows();
+      return (friend_single_city1_catherine_info.innerHTML = res);
+    });
   };
   friend_single_city1_jibblet.onclick = function () {
-    friendDetails('Jibblet', '/images/npcs/city1/catherine.png').then((res) => {
+    friendDetails(
+      CITY1_FRIENDS.JIBBLET,
+      '/images/npcs/city1/catherine.png'
+    ).then((res) => {
+      hideFriendRows();
       return (friend_single_city1_jibblet_info.innerHTML = res);
     });
-
-    hideFriendRows();
+  };
+  friend_single_city1_jonathan.onclick = function () {
+    friendDetails(
+      CITY1_FRIENDS.JONATHAN,
+      '/images/npcs/city1/catherine.png'
+    ).then((res) => {
+      hideFriendRows();
+      return (friend_single_city1_jonathan_info.innerHTML = res);
+    });
+  };
+  friend_single_city1_jt.onclick = function () {
+    friendDetails(CITY1_FRIENDS.JT, '/images/npcs/city1/catherine.png').then(
+      (res) => {
+        hideFriendRows();
+        return (friend_single_city1_jt_info.innerHTML = res);
+      }
+    );
+  };
+  friend_single_city1_karath.onclick = function () {
+    friendDetails(
+      CITY1_FRIENDS.KARATH,
+      '/images/npcs/city1/catherine.png'
+    ).then((res) => {
+      hideFriendRows();
+      return (friend_single_city1_karath_info.innerHTML = res);
+    });
+  };
+  friend_single_city1_matty.onclick = function () {
+    friendDetails(CITY1_FRIENDS.MATTY, '/images/npcs/city1/catherine.png').then(
+      (res) => {
+        hideFriendRows();
+        return (friend_single_city1_matty_info.innerHTML = res);
+      }
+    );
+  };
+  friend_single_city1_moses.onclick = function () {
+    friendDetails(CITY1_FRIENDS.MOSES, '/images/npcs/city1/catherine.png').then(
+      (res) => {
+        hideFriendRows();
+        return (friend_single_city1_moses_info.innerHTML = res);
+      }
+    );
+  };
+  friend_single_city1_nektarios.onclick = function () {
+    friendDetails(
+      CITY1_FRIENDS.NEKTARIOS,
+      '/images/npcs/city1/catherine.png'
+    ).then((res) => {
+      hideFriendRows();
+      return (friend_single_city1_nektarios_info.innerHTML = res);
+    });
+  };
+  friend_single_city1_newberry.onclick = function () {
+    friendDetails(
+      CITY1_FRIENDS.NEWBERRY,
+      '/images/npcs/city1/catherine.png'
+    ).then((res) => {
+      hideFriendRows();
+      return (friend_single_city1_newberry_info.innerHTML = res);
+    });
+  };
+  friend_single_city1_orpheus.onclick = function () {
+    friendDetails(
+      CITY1_FRIENDS.ORPHEUS,
+      '/images/npcs/city1/catherine.png'
+    ).then((res) => {
+      hideFriendRows();
+      return (friend_single_city1_orpheus_info.innerHTML = res);
+    });
+  };
+  friend_single_city1_ryan.onclick = function () {
+    friendDetails(CITY1_FRIENDS.RYAN, '/images/npcs/city1/catherine.png').then(
+      (res) => {
+        hideFriendRows();
+        return (friend_single_city1_ryan_info.innerHTML = res);
+      }
+    );
+  };
+  friend_single_city1_tanner.onclick = function () {
+    friendDetails(
+      CITY1_FRIENDS.TANNER,
+      '/images/npcs/city1/catherine.png'
+    ).then((res) => {
+      hideFriendRows();
+      return (friend_single_city1_tanner_info.innerHTML = res);
+    });
+  };
+  friend_single_city1_tsubaki.onclick = function () {
+    friendDetails(
+      CITY1_FRIENDS.TSUBAKI,
+      '/images/npcs/city1/catherine.png'
+    ).then((res) => {
+      hideFriendRows();
+      return (friend_single_city1_tsubaki_info.innerHTML = res);
+    });
+  };
+  friend_single_city1_tsuki.onclick = function () {
+    friendDetails(CITY1_FRIENDS.TSUKI, '/images/npcs/city1/catherine.png').then(
+      (res) => {
+        hideFriendRows();
+        return (friend_single_city1_tsuki_info.innerHTML = res);
+      }
+    );
+  };
+  friend_single_city1_victor.onclick = function () {
+    friendDetails(
+      CITY1_FRIENDS.VICTOR,
+      '/images/npcs/city1/catherine.png'
+    ).then((res) => {
+      hideFriendRows();
+      return (friend_single_city1_victor_info.innerHTML = res);
+    });
   };
 
   // hide friend details and show friend rows when clicked
